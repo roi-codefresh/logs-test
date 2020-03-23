4ab42ec3df8486a6b543a1f95a1a5348838faae3
Roi Kramer <roi.kramer@codefresh.io>
2020-03-23 09:33:14 +0200

    WiP

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+const { Base64Encode } = require('base64-stream');
 const fs = require('fs');
 
 try {
     const stream = fs.createReadStream('./war-and-peace.txt');
     const copyStream = fs.createWriteStream('./copy.txt');
-    stream.pipe(process.stdout);
-    stream.pipe(copyStream);
+    stream.pipe(new Base64Encode()).pipe(process.stdout);
+    stream.pipe(new Base64Encode()).pipe(copyStream);
 } catch (err) {
     console.error(err);
     process.exit(1);
